1919a8f2fe697c4a3c82b5ec0d113942565c9e52
Oscar Guarnizo <hoskarg9619@gmail.com>
2018-04-24 23:00:15 -0500

    Update .vpp .sql

--- Jump Create.sql
+++ Jump Create.sql
@@ -3,7 +3,6 @@ CREATE SEQUENCE userState_id_seq;
 CREATE SEQUENCE userJump_id_seq;
 CREATE SEQUENCE nationalIDType_id_seq;
 CREATE SEQUENCE location_id_seq;
-CREATE SEQUENCE ranking_id_seq;
 CREATE SEQUENCE job_id_seq;
 CREATE SEQUENCE employeeState_id_seq;
 CREATE SEQUENCE tagJump_id_seq;
@@ -22,29 +21,30 @@ CREATE TABLE Category (Id SERIAL NOT NULL, Nombre varchar(30) NOT NULL, Descript
 CREATE TABLE Chat (Id SERIAL NOT NULL, PRIMARY KEY (Id));
 CREATE TABLE ChatLine (Id SERIAL NOT NULL, IdChat int4 NOT NULL, IdUser int4 NOT NULL, LineText text NOT NULL, WrittenDate timestamp NOT NULL, PRIMARY KEY (Id));
 CREATE TABLE Comment (Id SERIAL NOT NULL, IdJob int4 NOT NULL, IdUser int4 NOT NULL, IdCommentParent int4 DEFAULT 0 NOT NULL, Description varchar(255), "Date" timestamp, PRIMARY KEY (Id));
-CREATE TABLE Employee (Id int4 NOT NULL, Ranking int4 NOT NULL, NumbJobsDone int4, PRIMARY KEY (Id));
+CREATE TABLE Employee (Id int4 NOT NULL, Ranking numeric(2, 1) NOT NULL, NumbJobsDone int4, PRIMARY KEY (Id));
 CREATE TABLE Employee_TagJump (IdEmployee int4 NOT NULL, IdTag int4 NOT NULL, PRIMARY KEY (IdEmployee, IdTag));
-CREATE TABLE EmployeeJob (IdEmployee int4 NOT NULL, IdJob int4 NOT NULL, RankErE int4, RankEEr int4, Salary numeric(9, 2) NOT NULL, CounterOfer numeric(9, 2), State int4 NOT NULL, PRIMARY KEY (IdEmployee, IdJob));
+CREATE TABLE EmployeeJob (IdEmployee int4 NOT NULL, IdJob int4 NOT NULL, RankErE numeric(2, 1), RankEEr numeric(2, 1), Salary numeric(9, 2) NOT NULL, CounterOfer numeric(9, 2), State int4 NOT NULL, PRIMARY KEY (IdEmployee, IdJob));
 CREATE TABLE EmployeeState (id SERIAL NOT NULL, description varchar(100) NOT NULL, PRIMARY KEY (id));
 CREATE TABLE Employer (Id int4 NOT NULL, Ranking int4 NOT NULL, SpentAmount numeric(7, 2) NOT NULL, JobsPosted int4 NOT NULL, PRIMARY KEY (Id));
 CREATE TABLE FavoritesJobs (IdEmployee int4 NOT NULL, IdJob int4 NOT NULL, PRIMARY KEY (IdEmployee, IdJob));
 CREATE TABLE Followers (IdFollower int4 NOT NULL, IdFollowed int4 NOT NULL, PRIMARY KEY (IdFollower, IdFollowed));
-CREATE TABLE Job (Id SERIAL NOT NULL, IdEmployeer int4 NOT NULL, Mode int4 NOT NULL, State int4 NOT NULL, IdLocation int4 NOT NULL, Title varchar(100) NOT NULL, Description varchar(500) NOT NULL, JobCost numeric(9, 2) NOT NULL, PhotoPath varchar(500) NOT NULL, DatePosted date NOT NULL, DateStart date NOT NULL, DateEnd date NOT NULL, DatePostEnd date NOT NULL, AvailablePercentage int4 NOT NULL, NumberApplicants int4 DEFAULT 1 NOT NULL, PRIMARY KEY (Id));
+CREATE TABLE Job (Id SERIAL NOT NULL, IdEmployeer int4 NOT NULL, Mode int4 NOT NULL, State int4 NOT NULL, IdLocation int4 NOT NULL, Title varchar(100) NOT NULL, Description varchar(500) NOT NULL, JobCost numeric(9, 2) NOT NULL, DatePosted date NOT NULL, DateStart date NOT NULL, DateEnd date NOT NULL, DatePostEnd date NOT NULL, AvailablePercentage int4 NOT NULL, NumberApplicants int4 DEFAULT 1 NOT NULL, PRIMARY KEY (Id));
 CREATE TABLE Job_TagJump (IdJob int4 NOT NULL, IdTag int4 NOT NULL, PRIMARY KEY (IdJob, IdTag));
 CREATE TABLE JobAddress (IdJob int4 NOT NULL, Direction int4 NOT NULL, Latitude numeric(10, 8) NOT NULL, Longitude numeric(10, 8) NOT NULL, PRIMARY KEY (IdJob));
 CREATE TABLE JobMode (Id SERIAL NOT NULL, Mode varchar(20) NOT NULL, PRIMARY KEY (Id));
 CREATE TABLE JobPhotos (IdJob int4 NOT NULL, Image bytea NOT NULL);
-CREATE TABLE JobState (Id SERIAL NOT NULL, State varchar(10) NOT NULL, PRIMARY KEY (Id));
+CREATE TABLE JobState (Id SERIAL NOT NULL, State varchar(30) NOT NULL, PRIMARY KEY (Id));
 CREATE TABLE Location (Id SERIAL NOT NULL, Country varchar(30) NOT NULL, City varchar(30) NOT NULL, PRIMARY KEY (Id));
 CREATE TABLE NationalIdentifierType (id SERIAL NOT NULL, description varchar(100) NOT NULL, PRIMARY KEY (id));
 CREATE TABLE PaypalAccount (Id int4 NOT NULL, IdUser int4 NOT NULL, Email varchar(25), PRIMARY KEY (Id, IdUser));
-CREATE TABLE Ranking (Id SERIAL NOT NULL, RankVal numeric(2, 1) NOT NULL, PRIMARY KEY (Id));
 CREATE TABLE TagJump (Id SERIAL NOT NULL, Name varchar(50) NOT NULL, Description varchar(100), CategoryId int4 NOT NULL, PRIMARY KEY (Id));
 CREATE TABLE TransactionJump (id SERIAL NOT NULL, idUser int4 NOT NULL, Idtype int4 NOT NULL, PRIMARY KEY (id));
 CREATE TABLE TransactionType (id SERIAL NOT NULL, name int4 NOT NULL, description int4 NOT NULL, PRIMARY KEY (id));
-CREATE TABLE UserJump (Id SERIAL NOT NULL, Id_Location int4 NOT NULL, IdState int4 NOT NULL, TypeNationalIdentifier int4 NOT NULL, NationalIdentifier int4 NOT NULL UNIQUE, Name varchar(30) NOT NULL, LastName varchar(30) NOT NULL, Email varchar(30) NOT NULL UNIQUE, Password varchar(15) NOT NULL, BirthDate date NOT NULL, Direction varchar(255), Gender char(1) NOT NULL, Nationality varchar(30) NOT NULL, AvailableMoney numeric(9, 2) NOT NULL, Nonce varchar(10) NOT NULL UNIQUE, PRIMARY KEY (Id));
+CREATE TABLE UserJump (Id SERIAL NOT NULL, IdLocation int4 NOT NULL, IdState int4 NOT NULL, TypeNationalIdentifier int4 NOT NULL, NationalIdentifier int4 NOT NULL UNIQUE, Name varchar(30) NOT NULL, LastName varchar(30) NOT NULL, Email varchar(30) NOT NULL UNIQUE, Password varchar(15) NOT NULL, BirthDate date NOT NULL, Direction varchar(255), Gender char(1) NOT NULL, Nationality varchar(30) NOT NULL, AvailableMoney numeric(9, 2) NOT NULL, Nonce varchar(10) NOT NULL UNIQUE, PRIMARY KEY (Id));
 CREATE TABLE UserStaff (IdUser int4 NOT NULL, About varchar(500) NOT NULL, PhotoPath varchar(225) NOT NULL, Cellphone varchar(20) NOT NULL, Image bytea NOT NULL, PRIMARY KEY (IdUser));
 CREATE TABLE UserState (Id SERIAL NOT NULL, State varchar(10) NOT NULL, PRIMARY KEY (Id));
+
+
 
 --Change serial with int
 ALTER TABLE Category ALTER COLUMN id TYPE int4; 
@@ -56,7 +56,6 @@ ALTER TABLE Job ALTER COLUMN id TYPE int4;
 ALTER TABLE JobMode ALTER COLUMN id TYPE int4; 
 ALTER TABLE JobState ALTER COLUMN id TYPE int4; 
 ALTER TABLE Location ALTER COLUMN id TYPE int4; 
-ALTER TABLE Ranking ALTER COLUMN id TYPE int4; 
 ALTER TABLE TagJump ALTER COLUMN id TYPE int4; 
 ALTER TABLE TransactionJump ALTER COLUMN id TYPE int4; 
 ALTER TABLE TransactionType ALTER COLUMN id TYPE int4; 
@@ -65,13 +64,11 @@ ALTER TABLE UserState ALTER COLUMN id TYPE int4;
 ALTER TABLE NationalIdentifierType ALTER COLUMN id TYPE int4; 
 
 --Constraint
-ALTER TABLE UserJump ADD CONSTRAINT FKUserJump139442 FOREIGN KEY (Id_Location) REFERENCES Location (Id);
+ALTER TABLE UserJump ADD CONSTRAINT FKUserJump853392 FOREIGN KEY (IdLocation) REFERENCES Location (Id);
 ALTER TABLE Employee ADD CONSTRAINT FKEmployee996956 FOREIGN KEY (Id) REFERENCES UserJump (Id);
 ALTER TABLE Employee_TagJump ADD CONSTRAINT FKEmployee_T827862 FOREIGN KEY (IdEmployee) REFERENCES Employee (Id);
 ALTER TABLE Employee_TagJump ADD CONSTRAINT FKEmployee_T803399 FOREIGN KEY (IdTag) REFERENCES TagJump (Id);
 ALTER TABLE Employer ADD CONSTRAINT FKEmployer996969 FOREIGN KEY (Id) REFERENCES UserJump (Id);
-ALTER TABLE Employer ADD CONSTRAINT FKEmployer290876 FOREIGN KEY (Ranking) REFERENCES Ranking (Id);
-ALTER TABLE Employee ADD CONSTRAINT FKEmployee290889 FOREIGN KEY (Ranking) REFERENCES Ranking (Id);
 ALTER TABLE Job ADD CONSTRAINT FKJob21280 FOREIGN KEY (Mode) REFERENCES JobMode (Id);
 ALTER TABLE Job ADD CONSTRAINT FKJob218444 FOREIGN KEY (State) REFERENCES JobState (Id);
 ALTER TABLE Job ADD CONSTRAINT FKJob67976 FOREIGN KEY (IdEmployeer) REFERENCES Employer (Id);
@@ -98,6 +95,10 @@ ALTER TABLE Followers ADD CONSTRAINT FKFollowers802736 FOREIGN KEY (IdFollowed) 
 ALTER TABLE TransactionJump ADD CONSTRAINT FKTransactio399029 FOREIGN KEY (Idtype) REFERENCES TransactionType (id);
 ALTER TABLE JobPhotos ADD CONSTRAINT FKJobPhotos830987 FOREIGN KEY (IdJob) REFERENCES Job (Id);
 
+
+--Constraints extras
+--Añadir el constraint para los rankings
+
 --Alter Tables with sequences
 ALTER TABLE Category ALTER COLUMN ID SET DEFAULT nextval('category_id_seq');
 ALTER TABLE Chat ALTER COLUMN ID SET DEFAULT nextval('chat_id_seq');
@@ -108,7 +109,6 @@ ALTER TABLE Job ALTER COLUMN ID SET DEFAULT nextval('job_id_seq');
 ALTER TABLE JobMode ALTER COLUMN ID SET DEFAULT nextval('jobMode_id_seq');
 ALTER TABLE JobState ALTER COLUMN ID SET DEFAULT nextval('jobState_id_seq');
 ALTER TABLE Location ALTER COLUMN ID SET DEFAULT nextval('location_id_seq');
-ALTER TABLE Ranking ALTER COLUMN ID SET DEFAULT nextval('ranking_id_seq');
 ALTER TABLE TagJump ALTER COLUMN ID SET DEFAULT nextval('tagJump_id_seq');
 ALTER TABLE TransactionJump ALTER COLUMN ID SET DEFAULT nextval('transaction_id_seq');
 ALTER TABLE TransactionType ALTER COLUMN ID SET DEFAULT nextval('transactionType_id_seq');
@@ -121,7 +121,6 @@ ALTER SEQUENCE userState_id_seq OWNED BY userState.id;
 ALTER SEQUENCE userJump_id_seq OWNED BY userJump.id;
 ALTER SEQUENCE nationalIDType_id_seq OWNED BY nationalIdentifierType.id;
 ALTER SEQUENCE location_id_seq OWNED BY location.id;
-ALTER SEQUENCE ranking_id_seq OWNED BY ranking.id;
 ALTER SEQUENCE job_id_seq OWNED BY job.id;
 ALTER SEQUENCE employeeState_id_seq OWNED BY employeeState.id;
 ALTER SEQUENCE tagJump_id_seq OWNED BY tagJump.id;
@@ -135,73 +134,72 @@ ALTER SEQUENCE chat_id_seq OWNED BY chat.id;
 ALTER SEQUENCE chatLine_id_seq OWNED BY chatLine.id;
 
 --DROP
-ALTER TABLE UserJump DROP CONSTRAINT FKUserJump139442;
-ALTER TABLE Employee DROP CONSTRAINT FKEmployee996956;
-ALTER TABLE Employee_TagJump DROP CONSTRAINT FKEmployee_T827862;
-ALTER TABLE Employee_TagJump DROP CONSTRAINT FKEmployee_T803399;
-ALTER TABLE Employer DROP CONSTRAINT FKEmployer996969;
-ALTER TABLE Employer DROP CONSTRAINT FKEmployer290876;
-ALTER TABLE Employee DROP CONSTRAINT FKEmployee290889;
-ALTER TABLE Job DROP CONSTRAINT FKJob21280;
-ALTER TABLE Job DROP CONSTRAINT FKJob218444;
-ALTER TABLE Job DROP CONSTRAINT FKJob67976;
-ALTER TABLE EmployeeJob DROP CONSTRAINT FKEmployeeJo204629;
-ALTER TABLE EmployeeJob DROP CONSTRAINT FKEmployeeJo483457;
-ALTER TABLE Job_TagJump DROP CONSTRAINT FKJob_TagJum687162;
-ALTER TABLE Job_TagJump DROP CONSTRAINT FKJob_TagJum990453;
-ALTER TABLE Job DROP CONSTRAINT FKJob50815;
-ALTER TABLE UserJump DROP CONSTRAINT FKUserJump698533;
-ALTER TABLE Comment DROP CONSTRAINT FKComment369264;
-ALTER TABLE Comment DROP CONSTRAINT FKComment890759;
-ALTER TABLE Comment DROP CONSTRAINT FKComment218269;
-ALTER TABLE UserStaff DROP CONSTRAINT FKUserStaff243811;
-ALTER TABLE JobAddress DROP CONSTRAINT FKJobAddress850771;
-ALTER TABLE ChatLine DROP CONSTRAINT FKChatLine684908;
-ALTER TABLE ChatLine DROP CONSTRAINT FKChatLine359812;
-ALTER TABLE TagJump DROP CONSTRAINT FKTagJump445836;
-ALTER TABLE EmployeeJob DROP CONSTRAINT FKEmployeeJo623179;
-ALTER TABLE UserJump DROP CONSTRAINT FKUserJump746039;
-ALTER TABLE FavoritesJobs DROP CONSTRAINT FKFavoritesJ937340;
-ALTER TABLE FavoritesJobs DROP CONSTRAINT FKFavoritesJ658512;
-ALTER TABLE Followers DROP CONSTRAINT FKFollowers802750;
-ALTER TABLE Followers DROP CONSTRAINT FKFollowers802736;
-ALTER TABLE TransactionJump DROP CONSTRAINT FKTransactio399029;
-ALTER TABLE JobPhotos DROP CONSTRAINT FKJobPhotos830987;
-DROP TABLE IF EXISTS Card CASCADE;
-DROP TABLE IF EXISTS Category CASCADE;
-DROP TABLE IF EXISTS Chat CASCADE;
-DROP TABLE IF EXISTS ChatLine CASCADE;
-DROP TABLE IF EXISTS Comment CASCADE;
-DROP TABLE IF EXISTS Employee CASCADE;
-DROP TABLE IF EXISTS Employee_TagJump CASCADE;
-DROP TABLE IF EXISTS EmployeeJob CASCADE;
-DROP TABLE IF EXISTS EmployeeState CASCADE;
-DROP TABLE IF EXISTS Employer CASCADE;
-DROP TABLE IF EXISTS FavoritesJobs CASCADE;
-DROP TABLE IF EXISTS Followers CASCADE;
-DROP TABLE IF EXISTS Job CASCADE;
-DROP TABLE IF EXISTS Job_TagJump CASCADE;
-DROP TABLE IF EXISTS JobAddress CASCADE;
-DROP TABLE IF EXISTS JobMode CASCADE;
-DROP TABLE IF EXISTS JobPhotos CASCADE;
-DROP TABLE IF EXISTS JobState CASCADE;
-DROP TABLE IF EXISTS Location CASCADE;
-DROP TABLE IF EXISTS NationalIdentifierType CASCADE;
-DROP TABLE IF EXISTS PaypalAccount CASCADE;
-DROP TABLE IF EXISTS Ranking CASCADE;
-DROP TABLE IF EXISTS TagJump CASCADE;
-DROP TABLE IF EXISTS TransactionJump CASCADE;
-DROP TABLE IF EXISTS TransactionType CASCADE;
-DROP TABLE IF EXISTS UserJump CASCADE;
-DROP TABLE IF EXISTS UserStaff CASCADE;
-DROP TABLE IF EXISTS UserState CASCADE;
+
 
 --Insert
-
 insert into userstate (state)values
-('Activo'),
-('Inactivo'),
-('Baneado');
+('Active'),
+('Inactive'),
+('Banned');
 
-select * from userstate
-insert into location
+insert into location(country,city) values
+('Ecuador','Quito'),
+('United States','New York'),
+('Colombia','Bogota');
+
+insert into NationalIdentifierType(description) values
+('Identification Card'),
+('Passport');
+
+insert into UserJump(id,idLocation,idstate,typeNationalIdentifier,nationalidentifier,name,lastname,email,password,birthdate,direction,gender,nationality,availablemoney,nonce) values
+(1,1,1,1,'123456789','Fernanda','Zapata','ferchz123@gmail.com','f123','1996-04-25','Quicentro Sur','F','Ecuadorian',1000.00,'A1H3F'),
+(2,1,2,1,'789465198','Camilo','Guitierrez','camilo566@gmail.com','c566','1998-04-25','Alameda','M','Ecuadorian',500.00,'W7G3R'),
+(3,1,3,1,'516549716','Laura','Rivera','laur894@gmail.com','l894','1995-03-21','Amaguaña','F','Ecuadorian',20.00,'T9K4I'),
+(4,2,1,2,'198952358','Enrique','Rivera','erivera879@gmail.com','e879','2000-05-17','Central Park','M','American',50.00,'U9D0E'),
+(5,3,1,1,'161698726','Jaime','Alban','jaimealba451@gmail.com','j451','1996-07-21','Iglesia de Veracruz','M','Colombian',700.00,'G79W5E');
+
+insert into Employee values
+(1,4.5,8),
+(2,3.5,9),
+(3,5.0,15),
+(4,4.3,8),
+(5,2.5,2);
+
+insert into Employer values
+(1,2.5,1500.00,2),
+(2,4.5,2500.00,3),
+(3,3.0,3000.00,4),
+(4,2.3,500.00,1),
+(5,4.5,10000.0,8);
+
+insert into jobState values 
+(default, 'Posted'),
+(default, 'Posted_InCourse'),
+(default, 'InCourse'),
+(default, 'FinishedNoPay'),
+(default, 'Paid');
+
+
+insert into jobmode values
+(default, 'Single'),
+(default, 'Single_Physical'),
+(default, 'Team'),
+(default, 'Team_Physical');
+
+insert into job values
+(default, 1, 1,1,1,'Web Page Development','No description yet', 10000, '2018-04-24','2018-04-30','2018-09-30','2018-04-29',30,0),
+(default, 2, 3,1,2,'Database design','No description yet', 100, '2018-03-24','2018-05-30','2018-06-30','2018-05-15',100,1),
+(default, 3, 2,1,3,'Walking my dog','Just walk my dog!', 10, '2018-04-24','2018-04-26','2018-04-26','2018-04-25',100,0);
+
+insert into employeestate values 
+(default, 'Applying'),
+(default, 'Working');
+
+insert into employeejob values
+(4,2,null,null,50,null,1);
+
+insert into favoritesjobs values
+(1,1),
+(1,2),
+(1,3);
+
