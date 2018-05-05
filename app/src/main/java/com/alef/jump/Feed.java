1bbfe184f5113c2f0a51bcfbebf10c94d9db826f
Fernando Zhapa <ferynando7@gmail.com>
2018-05-05 07:56:15 -0500

    Estructura de jobItem a diseñar

--- app/src/main/java/com/alef/jump/Feed.java
+++ app/src/main/java/com/alef/jump/Feed.java
@@ -1,6 +1,7 @@
 package com.alef.jump;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -15,7 +16,7 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 public class Feed extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, jobItem.OnFragmentInteractionListener{
 
 
 
@@ -47,27 +48,27 @@ public class Feed extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        mTextMessage = findViewById(R.id.message);
+        BottomNavigationView bottomBar = findViewById(R.id.navigation);
+        bottomBar.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        NavigationView lateralBar = findViewById(R.id.nav_view);
+        lateralBar.setNavigationItemSelectedListener(this);
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -112,6 +113,8 @@ public class Feed extends AppCompatActivity
         } else if (id == R.id.nav_myBusiness) {
 
         } else if (id == R.id.nav_addJob) {
+            Intent intent = new Intent(getApplicationContext(), AddJob.class);
+            startActivity(intent);
 
         } else if (id == R.id.nav_settings) {
 
@@ -122,5 +125,10 @@ public class Feed extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
