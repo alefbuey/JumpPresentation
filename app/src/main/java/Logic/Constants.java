172ca69a49b1a51a145d9cfd807e48c299766548
Fernando Zhapa <ferynando7@gmail.com>
2018-05-07 21:16:28 -0500

    Intentando implementar el manejo de imagenes

--- app/src/main/java/Logic/Constants.java
+++ app/src/main/java/Logic/Constants.java
@@ -10,8 +10,8 @@ public class Constants {
 
 
     //private static final String ip = "http://192.168.88.44"; //Oscar
-    //private static final String ip = "http://192.168.10.101"; //Fernando repetidor
-    private static final String ip = "http://192.168.0.108"; //Fernando emulador
+    private static final String ip = "http://192.168.10.101"; //Fernando repetidor
+    //private static final String ip = "http://192.168.0.108"; //Fernando emulador
 
 
 
--- app/src/main/java/com/alef/jump/jobItem.java
+++ app/src/main/java/com/alef/jump/jobItem.java
@@ -3,9 +3,13 @@ package com.alef.jump;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,21 +17,29 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import Logic.Constants;
 import Logic.GetRequest;
 
 public class jobItem extends Fragment {
 
-    ImageView imPhotoProf;
+    ImageView imPhotoProf, imShare, imAddFav;
     TextView tvProfileName, tvJobCost,tvJobName, tvNumDays;
     ImageView imPhotoJob;
-    Button btnAddFav, btnApply;
     LinearLayout llNumVac, llListaCateg;
+
+    boolean isFav = false;
 
     int id = 0;
     private static final String EXTRA_ID = "IDMETA";
@@ -70,19 +82,37 @@ public class jobItem extends Fragment {
         tvJobName = view.findViewById(R.id.tv_jobName);
         tvNumDays = view.findViewById(R.id.tv_numDaysAgo);
         imPhotoJob = view.findViewById(R.id.im_photoJob);
-        btnAddFav = view.findViewById(R.id.btn_addFav);
-        btnApply = view.findViewById(R.id.btn_apply);
+        imAddFav = view.findViewById(R.id.im_addFav);
+        imShare = view.findViewById(R.id.im_share);
         llNumVac = view.findViewById(R.id.ll_numVac);
         llListaCateg = view.findViewById(R.id.ll_listCateg);
 
 
-        btnApply.setOnClickListener(new View.OnClickListener() {
+        tvJobName.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getActivity(), JobView.class);
                 startActivity(intent);
             }
         });
+
+        imAddFav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!isFav)imAddFav.setImageDrawable(getResources().getDrawable(R.drawable.ic_favorite_filled));
+                else imAddFav.setImageDrawable(getResources().getDrawable(R.drawable.ic_favorite));
+                isFav = !isFav;
+            }
+        });
+
+        imShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "No social networks available",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
 
         GetRequest testReqJob = new GetRequest() {
             @Override
@@ -95,6 +125,14 @@ public class jobItem extends Fragment {
                     tvJobName.setText(dataJob.getString("title"));
                     tvJobCost.setText(dataJob.getString("jobcost"));
                     tvNumDays.setText(dataJob.getString("dateposted"));
+
+                    /*Tratamiento de la imagen del perfil */
+
+
+                    Bitmap bm = getImage("/var/www/html/pruebaImg/index.php?=1");
+
+                    imPhotoProf.setImageBitmap(bm);
+
 
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -132,4 +170,24 @@ public class jobItem extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+
+    public static Bitmap getImage(String url){
+        Bitmap img = null ;
+        try {
+            URL feedImage = new URL(url);
+            HttpURLConnection conn= (HttpURLConnection)feedImage.openConnection();
+            InputStream is = conn.getInputStream();
+            img = BitmapFactory.decodeStream(is);
+
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return img ;
+    }
+
 }
