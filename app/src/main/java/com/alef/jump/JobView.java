254c6b655f69d9dcaf530bba2a7ba1c0a4d66c46
Oscar Guarnizo <hoskarg9619@gmail.com>
2018-05-11 13:17:58 -0500

    cambios de interface y navegacion

--- app/src/main/java/com/alef/jump/JobView.java
+++ app/src/main/java/com/alef/jump/JobView.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -16,13 +17,16 @@ import org.json.JSONObject;
 import Logic.Constants;
 import Logic.Functions;
 import Logic.SendGetRequest;
+import People.User;
 
 
 public class JobView extends AppCompatActivity {
 
     private int id;
     private Double jobCost;
+    String TAG = "JobView";
 
+    String emailUser;
     TextView tvJobName, tvDesc, tvJobCost, tvStartDate, tvEndDate, tvNumVac;
 
     Button btnApply, btnList;
@@ -80,7 +84,7 @@ public class JobView extends AppCompatActivity {
                         JSONObject dataJob = jsonObject.getJSONObject("dataJob");
                         JSONObject dataUser = jsonObject.getJSONObject("dataUser");
                         //  JSONObject dataUserStaff = jsonObject.getJSONObject("dataUserStaff");
-
+                        emailUser = dataUser.getString("email");
                         int idEmployer = dataUser.getInt("id");
 
                         if(idEmployer==Globals.getInstance().getId()){
@@ -110,6 +114,74 @@ public class JobView extends AppCompatActivity {
 
         if(id > 0) {
             sendGetRequest.execute();
+        }
+
+    }
+
+    public void onClickProfile(View v){
+        if(emailUser!=null){
+            String url = Constants.getSelectUserProfile() + "?email=" + emailUser;
+
+            Log.e(TAG,url);
+
+            @SuppressLint("StaticFieldLeak") SendGetRequest sendGetRequest = new SendGetRequest(url) {
+                @Override
+                protected void onPostExecute(String response) {
+                    if (response != null) {
+                        JSONObject jsonObject = null;
+
+                        User userToPass = null;
+                        Log.e(TAG,response);
+
+                        try {
+                            jsonObject = new JSONObject(response);
+                            JSONObject jsonUser = (JSONObject) jsonObject.getJSONObject("user");
+                            JSONObject jsonUserStaff = (JSONObject) jsonObject.getJSONObject("userStaff");
+                            JSONObject jsonUserState = (JSONObject) jsonObject.getJSONObject("userState");
+                            JSONObject jsonUserNIType = (JSONObject) jsonObject.getJSONObject("userNIType");
+                            JSONObject jsonUserLocation = (JSONObject) jsonObject.getJSONObject("userLocation");
+                            JSONObject jsonUserPreferences = (JSONObject) jsonObject.getJSONObject("userPreferences");
+                            Log.e("TAG", jsonUser.toString());
+
+                            userToPass = new User(
+                                    jsonUser.getString("id"),
+                                    jsonUser.getString("email"),
+                                    jsonUser.getString("name"),
+                                    jsonUser.getString("lastname"),
+                                    jsonUserLocation.getString("country") + " - " + jsonUserLocation.getString("city"),
+                                    jsonUserState.getString("state"),
+                                    jsonUserNIType.getString("description"),
+                                    jsonUser.getString("nationalidentifier"),
+                                    jsonUser.getString("birthdate"),
+                                    jsonUser.getString("direction"),
+                                    jsonUser.getString("gender"),
+                                    jsonUser.getString("nationality"),
+                                    jsonUser.getString("availablemoney"),
+                                    jsonUser.getString("rank"),
+                                    jsonUserPreferences.getString("preferences"),
+                                    jsonUserStaff.getString("about"),
+                                    jsonUserStaff.getString("cellphone")
+                            );
+                            Log.e("TAG", userToPass.toString());
+                            Intent i = new Intent(getApplicationContext(), Profile.class);
+                            i.putExtra("modo",2);
+                            i.putExtra("user", userToPass);
+                            startActivity(i);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Failed Connection", Toast.LENGTH_LONG).show();
+                    }
+                }
+            };
+
+
+            sendGetRequest.execute();
+        }else{
+            Toast.makeText(getApplicationContext(),"Information of User not Found",Toast.LENGTH_LONG);
         }
 
     }
